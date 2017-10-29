0c7b3c4166fac2a4160661b2f4ac228b0cc5d279
Jose Meyer <josef.meyer89@gmail.com>
2017-10-29 08:44:22 -0200

    Project structure

---
+++ profilizer-service/src/main/java/com/profilizer/ProfilizerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.profilizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilizerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilizerServiceApplication.class, args);
+	}
+}
